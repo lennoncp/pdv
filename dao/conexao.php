9b6e28d4f6e826f9e35c2121aa2170d1fcfd0064
lennoncp <lennon.palmerio@gmail.com>
2018-08-28 19:25:23 -0300

    Primeiro Commit

--- dao/conexao.php
+++ dao/conexao.php
@@ -3,4 +3,10 @@
     $conexao = mysqli_connect("127.0.0.1", "root", "", "pdv");
     mysqli_select_db($conexao, "pdv");
 
+    # Aqui está o segredo
+mysqli_query($conexao, "SET NAMES 'utf8'");
+mysqli_query($conexao, 'SET character_set_connection=utf8');
+mysqli_query($conexao, 'SET character_set_client=utf8');
+mysqli_query($conexao, 'SET character_set_results=utf8');
+
 ?>
